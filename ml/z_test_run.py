11883e037b516a1bce72672b2c6d399aba8a6ba7
João Monteiro <monteiro.joao.ps@gmail.com>
2022-04-08 20:05:27 +0100

    update mlproject

--- ml/z_test_run.py
+++ ml/z_test_run.py
@@ -5,7 +5,7 @@ import os
 cwd = os.getcwd()
 
 print(cwd)
-project_uri = "."
+project_uri = "https://github.com/Portugapt/Closer-Challenge#ml"
 params = {"log_this_string": "Test_String"}
 
-mlflow.run(project_uri, parameters=params, entry_point = 'mlflow_test')
+mlflow.run(project_uri, version ='main', parameters=params, entry_point = 'main')
